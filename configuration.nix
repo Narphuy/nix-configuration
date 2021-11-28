ae073c18162c1aa73af4d713c47bc19a420e24ff
Narph <92280556+Narphuy@users.noreply.github.com>
2021-11-28 23:17:03 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,113 +1,96 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports = [ # Include the results of the hardware scan.
+  imports = [ # results of hardware scan
       ./hardware-configuration.nix
   ];
   boot.extraModulePackages = [
-    config.boot.kernelPackages.rtl8821ce
+    config.boot.kernelPackages.rtlwifi_new
   ];
 
-  # Use the systemd-boot EFI boot loader.
+  # systemd boot
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "hpxos"; # Define your hostname.
+  networking.hostName = "nix";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/New_York";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
+  # what the fuck is a DHCP
   networking.useDHCP = false;
   networking.interfaces.wlo1.useDHCP = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
+  # locale
   i18n.defaultLocale = "en_US.UTF-8";
+
+  # console
   console = {
     font = "Lat2-Terminus16";
     keyMap = "us";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  # x11
+  services.xserver = {
+    enable = true;
 
+    displayManager = {
+      sddm.enable = true;
+    };
 
-  # Enable the Plasma 5 Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+    desktopManager = {
+      plasma5.enable = true;
+    };
 
-  # Configure keymap in X11
-  services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
+    layout = "us";
+    libinput.enable = true;
+  };
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true; # ensuring gtk works
+    extraPackages = with pkgs; [
+      swaylock
+      swayidle
+      wl-clipboard
+      mako
+    ];
+  };
 
-  # Enable sound.
+  # fonts
+  fonts.fonts = with pkgs; [
+    nerdfonts
+  ];
+
+  # sound
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.prae = {
     isNormalUser = true;
     description = "Prae";
     extraGroups = [ 
       "wheel"
       "video"
-      "networkmanger"
+      "networkmanager"
     ];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     wget
     firefox
     brightnessctl
     git
     neofetch
+    alacritty
+    polybar
+    rofi
+    waybar
+    wofi
+    curl
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # you should probably not change this
   system.stateVersion = "21.11"; # Did you read the comment?
-
 }
